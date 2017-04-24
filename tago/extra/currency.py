3bf1b51d7bfb51ba5cfe241d0990e924dd8120fb
Madhav Bhatt <madhav.bhatt73@gmail.com>
2017-04-23 21:03:35 -0400

    Add files via upload
    Distance and Currency

---
+++ tago/extra/currency.py
@@ -0,0 +1,18 @@
+from pygments import console
+from tago import analysis as Analysis
+from tago import extra as Extra
+
+def myanalysis(context, scope):
+    api_key = '217d254d86b0e2d8359be6c46d2742f1'
+    currency = Extra('currency', api_key)
+
+    fro = 'USD'
+    to = 'BRL'
+
+    currency.convert(fro, to)
+    try:
+        print(console.log)
+    except ValueError:
+        print(console.log)
+
+currency = Analysis(myanalysis, 'c89f0d50-38e2-11e6-966e-b94d760acc7d')
---
+++ tago/extra/distance.py
@@ -0,0 +1,20 @@
+from pygments import console
+from tago import analysis as Analysis
+from tago import extra as Extra
+
+
+def myanalysis(context, scope):
+    api_key = 'AIzaSyAKkEKku8XrbzBJFDLDjtiacS4bRlF0E8s'  # API that you can get in the google website
+    distance = Extra('distance', api_key)
+    origins = "New York, NY, USA"
+    destinations = "Washington, DC, USA"
+    language = 'EN'
+    mode = 'driving'
+
+    distance.measure(origins, destinations, language, mode)
+    try:
+        print(console.log)
+    except ValueError:
+        print(console.log)
+
+module = Analysis(myanalysis, 'c89f0d50-38e2-11e6-966e-b94d760acc7d')
